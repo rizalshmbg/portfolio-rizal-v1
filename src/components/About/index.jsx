2fc3e006c4a72be9ffa2c36312c2bdb35c3be40b
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-25 10:38:46 +0700

    complete about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faCss3,
+	faGitAlt,
+	faHtml5,
+	faJsSquare,
+	faNodeJs,
+	faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const aboutNameArray = ['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']
 
 const About = () => {
+	const [letterClass, setLetterClass] = useState('text-animate')
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLetterClass('text-animate-hover')
+		}, 3000)
+	}, [])
+
 	return (
 		<div className='container about-page'>
 			<div className='text-zone'>
 				<h1>
-					<AnimatedLetters strArray={aboutNameArray} idx={15} />
+					<AnimatedLetters
+						strArray={aboutNameArray}
+						letterClass={letterClass}
+						idx={15}
+					/>
 				</h1>
 				<p>
 					I&#39;m a very ambitious front-end developer looking for a role in an
@@ -24,6 +46,29 @@ const About = () => {
 					person, father of a beautiful daughter, a sports fanatic, photography
 					enthusiast, and tech-obsessed!!!
 				</p>
+			</div>
+
+			<div className='stage-cube-cont'>
+				<div className='cubespinner'>
+					<div className='face1'>
+						<FontAwesomeIcon icon={faHtml5} color='#F06529' />
+					</div>
+					<div className='face2'>
+						<FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+					</div>
+					<div className='face3'>
+						<FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+					</div>
+					<div className='face4'>
+						<FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+					</div>
+					<div className='face5'>
+						<FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+					</div>
+					<div className='face6'>
+						<FontAwesomeIcon icon={faNodeJs} color='#3c873a' />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
