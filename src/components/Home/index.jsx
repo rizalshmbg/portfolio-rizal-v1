279a03d1d61e7e6bbd25ccaa5f0f36f23a2800dc
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-25 19:49:26 +0700

    add loader of pacman

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
 import './index.scss'
+import Loader from 'react-loaders'
 
 const Home = () => {
 	const [letterClass, setLetterClass] = useState('text-animate')
@@ -32,42 +33,45 @@ const Home = () => {
 	}, [])
 
 	return (
-		<div className='container home-page'>
-			<div className='text-zone'>
-				<h1>
-					<span className={letterClass}>H</span>
-					<span className={`${letterClass} _12`}>i</span>
-					<span className={`${letterClass} _13`}>,</span>
-					<br />
-					<span className={`${letterClass} _14`}>I</span>
-					<span className={`${letterClass} _15`}>&#39;</span>
-					<span className={`${letterClass} _16`}>m</span>
-					{/* <span className={`${letterClass} _17`}>&nbsp;</span> */}
-					{/* <AnimatedLetters
+		<>
+			<div className='container home-page'>
+				<div className='text-zone'>
+					<h1>
+						<span className={letterClass}>H</span>
+						<span className={`${letterClass} _12`}>i</span>
+						<span className={`${letterClass} _13`}>,</span>
+						<br />
+						<span className={`${letterClass} _14`}>I</span>
+						<span className={`${letterClass} _15`}>&#39;</span>
+						<span className={`${letterClass} _16`}>m</span>
+						{/* <span className={`${letterClass} _17`}>&nbsp;</span> */}
+						{/* <AnimatedLetters
 						letterClass={letterClass}
 						strArray={firstNameArray}
 						idx={18}
 					/> */}
-					<img src={LogoTitle} alt='Logo' />
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={lastNameArray}
-						idx={17}
-					/>
-					<br />
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={jobArray}
-						idx={25}
-					/>
-				</h1>
-				<h2>Fresh Graduate of BCS / Jr. Frontend Web Developer</h2>
-				<Link to='/contact' className='flat-button'>
-					CONTACT ME
-				</Link>
+						<img src={LogoTitle} alt='Logo' />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={lastNameArray}
+							idx={17}
+						/>
+						<br />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={jobArray}
+							idx={25}
+						/>
+					</h1>
+					<h2>Fresh Graduate of BCS / Jr. Frontend Web Developer</h2>
+					<Link to='/contact' className='flat-button'>
+						CONTACT ME
+					</Link>
+				</div>
+				<Logo />
 			</div>
-			<Logo />
-		</div>
+			<Loader type='pacman' />
+		</>
 	)
 }
 export default Home
